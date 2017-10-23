223429e295b3c66a8ffe4e7248e16d8d686639d3
Artur Kowalczyk <fizjokowalczyk@gmail.com>
2017-10-23 22:24:06 +0200

    change h1 in nav and color logo

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+      sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.scss'
+        }
+      }
+    },
+      imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    }
+},
+      watch: {
+    scripts: {
+        files: ['sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    }
+},
+browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        './css/*.css',
+                        './*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+  });
+  // Load the plugins tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync',"sass",'watch']);
+};
